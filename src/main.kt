8d3ae7e36a9c54032d18bf77655dadc2ee4380f8
Raphael.AmorimAlex <Raphael.amorimalex@gmail.com>
2021-02-22 20:18:03 -0300

    Aula 8 Diferença Empty e blank

---
+++ src/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val s = ""
+    val v = "  "
+    println(s.isEmpty())
+    println(s.isBlank())
+    println(s.isNullOrBlank() || s.isNullOrEmpty())
+    println(v.isEmpty())
+    println(v.isBlank())
+    println(s.length)
+    println(v.length)
+}
